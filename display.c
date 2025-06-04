c08fa7e07e306ec63eed16878dfcff440fc4bf1e
Владимир Чирикало <chirikalo@atomtex.com>
2025-06-04 15:04:51 +0300

    04.06.2025 для отправки, предыдущая не очень

--- display.c
+++ display.c
@@ -754,36 +754,27 @@ void Display_turnON(void)
 	display.bLCDON = TRUE;
 
 	
-<<<<<<< HEAD
 	PowerControl_EMC_ON();
         CLR_RS;
 	DisplayData = SEL_DB2;  //8-bit!!!!!!         
 	SET_RS;
 
         CLR_RS;
-=======
-	
-	CLR_RS;
->>>>>>> 0bfbe0b12f14c035f3b1fcb24871ac45f2a0cb08
 	DisplayData = STB;
 	SET_RS;
 	DisplayData = 0x00;
 	DisplayData = 0x00;     //stb off
-<<<<<<< HEAD
 	CLR_RS;
 	DisplayData = DDC;
 	SET_RS;
 	DisplayData = 0x88;     //Front Porch and BBack Porch
 	DisplayData = 0x28;     //320*240
 	
-=======
->>>>>>> 0bfbe0b12f14c035f3b1fcb24871ac45f2a0cb08
         CLR_RS;
 	DisplayData = OSC_RADJ;
 	SET_RS;
 	DisplayData = 0x00;
 	DisplayData = 0x1a;     //x 0.877 если выше мигают буквы на русском
-<<<<<<< HEAD
         CLR_RS;
 	DisplayData = SODR;
 	SET_RS;
@@ -810,26 +801,12 @@ void Display_turnON(void)
 	SET_RS;
 	DisplayData = 0x00;
 	DisplayData = 0x0b;     //-4.2 V
-=======
-
->>>>>>> 0bfbe0b12f14c035f3b1fcb24871ac45f2a0cb08
         
         CLR_RS;
 	DisplayData = DC2;
 	SET_RS;
 	DisplayData = 0x00;
 	DisplayData = 0x02;     //TEMON = 1
-<<<<<<< HEAD
-=======
-//	CLR_RS;
-//	DisplayData = SODR;
-//	SET_RS;
-//	DisplayData = 0x00;
-//	DisplayData = 0x04;     //Slow or medium 
-        
-	Display_Init_8bit_262k();
-	Display_clearScreen();
->>>>>>> 0bfbe0b12f14c035f3b1fcb24871ac45f2a0cb08
 	
         
 	
@@ -1083,29 +1060,7 @@ void Display_init(void)
 	//===== Иниацилизация и включение дисплея ============================
 	CLR_NRES;
 	SET_NRES;
-        PowerControl_EMC_ON();
-        CLR_RS;
-	DisplayData = DISP_ON;
-	SET_RS;
-	DisplayData = 0x00;
-	DisplayData = 0x00;
-
-
-	CLR_RS;
-	DisplayData = DDC;
-	SET_RS;
-	DisplayData = 0x88;     //Front Porch and BBack Porch
-	DisplayData = 0x28;     //320*240
-        CLR_RS;
-	DisplayData = VGH;
-	SET_RS;
-	DisplayData = 0x00;
-	DisplayData = 0x0a;     //4.2 V
-        CLR_RS;
-	DisplayData = VGL;
-	SET_RS;
-	DisplayData = 0x00;
-	DisplayData = 0x0a;     //-4.2 V
+	
 	
 	Display_turnON();
 	
